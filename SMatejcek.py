afbc439d4fa165d2f7709122e0fb537ae29d3504
Matejcek <Steven.Matejcek@McKesson.com>
2018-01-26 14:43:54 -0500

    Misadventure with Nans

--- SMatejcek.py
+++ SMatejcek.py
@@ -32,7 +32,7 @@ X = df[["VehYear", "ModelNumeric", "MakeNumeric", "TransmissionNumeric", "SizeNu
 X.dropna(axis=0, how='any', inplace=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 model = GradientBoostingClassifier().fit(X_train, y_train)
-print("SCORE: {}".format(model.score(X_test, y_test))
+print("SCORE: {}".format(model.score(X_test, y_test)))
 
 cv = KFold(n_splits = 10)
 
